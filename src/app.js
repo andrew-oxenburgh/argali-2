2481cf2962b7f91267031b15a9a3009d237fee0b
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2018-06-28 17:18:25 +1200

    added scss loader

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import {PLATFORM} from 'aurelia-pal';
+import './styles.scss'
 
 export class App {
     configureRouter(config, router) {
         config.title = 'Argali';
         config.map([
-          {route: 'staff', moduleId: PLATFORM.moduleName('staff'), title: 'Staff'},
-          {route: 'report', moduleId: PLATFORM.moduleName('report'), title: 'Report'},
-          {route: '', moduleId: PLATFORM.moduleName('no-selection'), title: 'Select'},
-          {route: 'contacts/:id', moduleId: PLATFORM.moduleName('contact-detail'), name: 'contacts'}
+            {route: 'staff', moduleId: PLATFORM.moduleName('staff'), title: 'Staff'},
+            {route: 'report', moduleId: PLATFORM.moduleName('report'), title: 'Report'},
+            {route: '', moduleId: PLATFORM.moduleName('no-selection'), title: 'Select'},
+            {route: 'contacts/:id', moduleId: PLATFORM.moduleName('contact-detail'), name: 'contacts'}
         ]);
 
         this.router = router;
--- src/main.js
+++ src/main.js
@@ -10,15 +10,15 @@ Bluebird.config({ warnings: { wForgottenReturn: false } });
 
 export function configure(aurelia) {
     aurelia.use
-    .standardConfiguration()
-    .feature(PLATFORM.moduleName('resources/index'));
+        .standardConfiguration()
+        .feature(PLATFORM.moduleName('resources/index'));
 
-  // Uncomment the line below to enable animation.
-  // aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
-  // if the css animator is enabled, add swap-order="after" to all router-view elements
+    // Uncomment the line below to enable animation.
+    // aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
+    // if the css animator is enabled, add swap-order="after" to all router-view elements
 
-  // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-  // aurelia.use.plugin(PLATFORM.moduleName('aurelia-html-import-template-loader'));
+    // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+    // aurelia.use.plugin(PLATFORM.moduleName('aurelia-html-import-template-loader'));
 
     if (environment.debug) {
         aurelia.use.developmentLogging();
